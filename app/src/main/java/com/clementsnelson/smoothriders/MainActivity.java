22cbc1e03a466f98a0b5690fb3d244b403840026
Daniel Clements <djc4810@uncw.edu>
2021-10-14 16:08:08 -0400

    renamed application

--- app/src/main/java/com/clementsnelson/smoothriders/MainActivity.java
+++ app/src/main/java/com/clementsnelson/smoothriders/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapplication;
+package com.clementsnelson.smoothriders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.smoothriders.R;
 
 public class MainActivity extends AppCompatActivity {
 
